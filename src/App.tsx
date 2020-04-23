32cc026b605044a6524e3cbb5b1aca9f2239998b
Benjamin Petersen <git@benjaminpetersen.com>
2020-04-22 20:10:51 -0700

    Run prettier on tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import TinyLineChart from './TinyChart';
-import ThreeDimScatterChart from './ScatterChart';
-
+import TinyLineChart from "./TinyChart";
+import ThreeDimScatterChart from "./ScatterChart";
 
 function App() {
   return (
@@ -12,15 +11,11 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <TinyLineChart />
-        <p>
-          Welcome to my first react app: A Bitcoin Comparison
-        </p>
+        <p>Welcome to my first react app: A Bitcoin Comparison</p>
       </header>
 
       <ThreeDimScatterChart />
-
     </div>
-
   );
 }
 
--- src/ScatterChart.tsx
+++ src/ScatterChart.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import Select from 'react-select';
-import {ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import React from "react";
+import Select from "react-select";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  ZAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
-import btc_raw from './data/btc.json';
-import eth_raw from './data/eth.json';
-import ltc_raw from './data/ltc.json';
-import xrp_raw from './data/xrp.json';
+import btc_raw from "./data/btc.json";
+import eth_raw from "./data/eth.json";
+import ltc_raw from "./data/ltc.json";
+import xrp_raw from "./data/xrp.json";
 // example obj structure
 // [{
 //     "price": 3685.14,
@@ -17,103 +26,128 @@ import xrp_raw from './data/xrp.json';
 // Colors inspired by
 // https://www.schemecolor.com/tomorrow.php
 const data = {
-    btc: {
-        value: 'btc',
-        label: 'Bitcoin',
-        color: '#FFB31C',
-        raw: btc_raw,
-    },
-    eth: {
-        value: 'eth',
-        label: 'Ethereum',
-        color: '#432392',
-        raw: eth_raw,
-    },
-    ltc: {
-        value: 'ltc',
-        label: 'Litecoin',
-        color: '#EC46C3',
-        raw: ltc_raw,
-    },
-    xrp: {
-        value: 'xrp',
-        label: 'Ripple',
-        color: '#4473F6',
-        raw: xrp_raw,
-    },
-}
+  btc: {
+    value: "btc",
+    label: "Bitcoin",
+    color: "#FFB31C",
+    raw: btc_raw,
+  },
+  eth: {
+    value: "eth",
+    label: "Ethereum",
+    color: "#432392",
+    raw: eth_raw,
+  },
+  ltc: {
+    value: "ltc",
+    label: "Litecoin",
+    color: "#EC46C3",
+    raw: ltc_raw,
+  },
+  xrp: {
+    value: "xrp",
+    label: "Ripple",
+    color: "#4473F6",
+    raw: xrp_raw,
+  },
+};
 
 function findAveragePrice(raw) {
-    const sum = raw.reduce((a,b) => a + b.price, 0);
-    const avg = (sum / data.btc.raw.length) || 0;
-    return (Math.round(avg * 100) / 100).toFixed(2);
+  const sum = raw.reduce((a, b) => a + b.price, 0);
+  const avg = sum / data.btc.raw.length || 0;
+  return (Math.round(avg * 100) / 100).toFixed(2);
 }
 
 // Select dropdown options for comparison
 const options = [data.eth, data.ltc, data.xrp];
 
 class ThreeDimScatterChart extends React.Component {
-    state = {
-        // default ethereum
-        selectedOption: data.eth,
-    };
+  state = {
+    // default ethereum
+    selectedOption: data.eth,
+  };
 
-    handleChange = selectedOption => {
-        this.setState(
-            { selectedOption },
-            () => console.log(`Option selected:`, this.state.selectedOption)
-        );
-    };
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption }, () =>
+      console.log(`Option selected:`, this.state.selectedOption)
+    );
+  };
 
-	render () {
-      return (
-        <div className='Scatter'>
+  render() {
+    return (
+      <div className="Scatter">
+        <ScatterChart
+          width={880}
+          height={420}
+          margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
+        >
+          <XAxis type="number" dataKey={"score"} name="Google Trend Score" />
+          <YAxis
+            yAxisId="left"
+            type="number"
+            dataKey={"price"}
+            name="Price"
+            unit="$"
+            stroke="#8884d8"
+          />
+          <YAxis
+            yAxisId="right"
+            type="number"
+            dataKey={"price"}
+            name="Price"
+            unit="$"
+            orientation="right"
+            stroke={this.state.selectedOption.color}
+          />
 
-            <ScatterChart width={880} height={420} margin={{top: 10, right: 10, bottom: 10, left: 10}}>
+          <ZAxis
+            type="number"
+            dataKey={"ratio"}
+            name="Low/High Ratio"
+            scale="auto"
+          />
 
-                <XAxis type="number" dataKey={'score'} name='score' />
-                <YAxis yAxisId="left" type="number" dataKey={'price'} name='Price' unit='$' stroke="#8884d8" />
-                <YAxis 
-                    yAxisId="right"
-                    type="number"
-                    dataKey={'price'}
-                    name='Price'
-                    unit='$'
-                    orientation="right"
-                    stroke={this.state.selectedOption.color}
-                    />
+          <CartesianGrid />
+          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+          <Legend iconType="triangle" />
 
-                <ZAxis type="number" dataKey={'ratio'} name='ratio' scale='auto'/>
+          <Scatter
+            yAxisId="left"
+            name={
+              data.btc.label +
+              " (avg. price: $" +
+              findAveragePrice(data.btc.raw) +
+              ")"
+            }
+            data={data.btc.raw}
+            fill={data.btc.color}
+            shape="circle"
+          />
+          <Scatter
+            yAxisId="right"
+            name={
+              this.state.selectedOption.label +
+              " (avg. price: $" +
+              findAveragePrice(this.state.selectedOption.raw) +
+              ")"
+            }
+            data={this.state.selectedOption.raw}
+            fill={this.state.selectedOption.color}
+            shape="triangle"
+          />
+        </ScatterChart>
 
-                <CartesianGrid />
-                <Tooltip cursor={{strokeDasharray: '3 3'}}/>
-                <Legend iconType='triangle' />
+        <p>
+          <code>Compare Bitcoin against...</code>
+        </p>
 
-                <Scatter 
-                    yAxisId='left'
-                    name={data.btc.label + ' (avg. price: $' + findAveragePrice(data.btc.raw) + ')'}
-                    data={data.btc.raw}
-                    fill={data.btc.color}
-                    shape='circle'
-                    />
-                <Scatter 
-                    yAxisId='right'
-                    name={this.state.selectedOption.label + ' (avg. price: $' + findAveragePrice(this.state.selectedOption.raw) + ')'}
-                    data={this.state.selectedOption.raw} 
-                    fill={this.state.selectedOption.color}
-                    shape="triangle"
-                    />
-            </ScatterChart>
-
-            <p><code>Compare Bitcoin against...</code></p>
-
-            <Select 
-                className='Scatter-select'
-                defaultValue={this.state.selectedOption}
-                onChange={this.handleChange}
-                options={options}
-                />
-        </div>
+        <Select
+          className="Scatter-select"
+          defaultValue={this.state.selectedOption}
+          onChange={this.handleChange}
+          options={options}
+        />
+      </div>
     );
   }
 }
